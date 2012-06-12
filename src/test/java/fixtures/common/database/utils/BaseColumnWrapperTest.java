a17c957cca7c5d6f8b64c22687ccc6a6de8553dd
ltricard <ltricard@ubuntu.(none)>
2012-06-12 14:30:07 +0200

    [LAT] remet les tests

--- src/test/java/fixtures/common/database/utils/BaseColumnWrapperTest.java
+++ src/test/java/fixtures/common/database/utils/BaseColumnWrapperTest.java
@@ -29,6 +29,6 @@ public class BaseColumnWrapperTest {
         assertThat(baseColumnNames.size(), is(FromXToDatatableEnum.values().length));
         assertThat(baseColumnNames, JUnitMatchers.hasItems(FromXToDatatableEnum.A.getDatatableColumnName(),
                 FromXToDatatableEnum.B.getDatatableColumnName(), FromXToDatatableEnum.C.getDatatableColumnName(),
-                FromXToDatatableEnum.E.getDatatableColumnName()));
+                FromXToDatatableEnum.D.getDatatableColumnName()));
     }
 }
