23d27a11aa85c59b21edd00d7a51829fea934951
clescot <lescot.charles@gmail.com>
2012-06-29 16:47:09 +0200

    [CLE] extends an Exception and not Throwable

--- src/main/java/fixtures/common/steputils/IllegalStepCallException.java
+++ src/main/java/fixtures/common/steputils/IllegalStepCallException.java
@@ -1,6 +1,6 @@
 package fixtures.common.steputils;
 
-public class IllegalStepCallException extends Throwable {
+public class IllegalStepCallException extends Exception {
     public IllegalStepCallException(final String stepToCallBefore) {
         super("Ce step est lié au step : \"" + stepToCallBefore + "\" qu'il faut appeler avant!");
     }
