5cf2e6b3a765c7c63b57804f173fdbcb0f1e75c6
clescot <lescot.charles@gmail.com>
2012-06-29 16:44:52 +0200

    [CLE] add a private constructor

--- src/main/java/fixtures/common/datatable/DatatableUtils.java
+++ src/main/java/fixtures/common/datatable/DatatableUtils.java
@@ -7,6 +7,8 @@ import cucumber.table.DataTable;
 
 public class DatatableUtils {
 
+    private DatatableUtils(){}
+
     public static DataTable getDatatable(List<String> headers, List<List<String>> values){
         return DataTable.create(values, Locale.getDefault(),headers.toArray(new String[headers.size()]));
     }
