c888c474b37b0765ec08d87f46501d7a783ed6a1
clescot <lescot.charles@gmail.com>
2012-06-29 16:45:38 +0200

    [CLE] add a private constructor

--- src/main/java/fixtures/common/transformers/Label.java
+++ src/main/java/fixtures/common/transformers/Label.java
@@ -3,6 +3,9 @@ package fixtures.common.transformers;
 import org.apache.commons.lang.StringUtils;
 
 public class Label {
+
+    private Label(){}
+    
     public static String cleanLabel(String label) {
         // le caractère à remplacer est le caractère espace de Format (' ') qui n'est pas un espace
         // classique mais un espace insécable
