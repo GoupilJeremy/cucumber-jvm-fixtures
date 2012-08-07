6297c5d2d2e63c793b6c39ec829a4956252e5a86
ltricard <ltricard@ubuntu.(none)>
2012-08-07 11:56:39 +0200

    [LAT] ajout de tests sur FileTransformer

--- src/main/java/fixtures/common/transformers/FileTransformer.java
+++ src/main/java/fixtures/common/transformers/FileTransformer.java
@@ -6,6 +6,7 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.common.base.Preconditions;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
 import com.google.common.io.Files;
@@ -18,39 +19,52 @@ import gherkin.formatter.model.DataTableRow;
 public class FileTransformer extends AbstractDataTableTransformer<File> {
     private static final Charset CHARSET = Charset.forName("ISO-8859-15");
 
+    private static final String SEPARATOR = "\t";
+
     public FileTransformer(DataTable dataTable) {
         super(dataTable);
     }
 
     @Override
     protected List<DataTableRow> buildRowForDataTable(final File file, final List<DataTableRow> rows) {
+        Preconditions.checkArgument(file != null, "le fichier ne peut ëtre null");
+        Preconditions.checkArgument(file.exists(), "le fichier doit exister");
         final List<DataTableRow> dataTableRows;
         try {
-            dataTableRows = Files.readLines(file, CHARSET, new LineProcessor<List<DataTableRow>>() {
-                private List<DataTableRow> dataTableRows = new ArrayList<DataTableRow>();
-
-                private int counter = 0;
-
-                @Override
-                public boolean processLine(final String line) throws IOException {
-                    final Splitter splitter = Splitter.on("\t");
-                    final Iterable<String> cells = splitter.split(line);
-                    DataTableRow dataTableRow = new DataTableRow(new ArrayList<Comment>(), Lists.newArrayList(cells),
-                            counter);
-                    dataTableRows.add(dataTableRow);
-                    counter++;
-                    return true;
-                }
-
-                @Override
-                public List<DataTableRow> getResult() {
-                    return dataTableRows;
-                }
-            });
+            dataTableRows = Files.readLines(file, CHARSET, newLineProcessor());
         } catch (IOException e) {
             throw new CucumberException("Test cucumber", e);
         }
 
         return dataTableRows;
     }
+
+    protected LineProcessor<List<DataTableRow>> newLineProcessor() {
+        return new InnerLineProcessor();
+    }
+
+    // =============================================================================================
+    // inner class
+    // =============================================================================================
+    
+    private static class InnerLineProcessor implements LineProcessor<List<DataTableRow>> {
+        private List<DataTableRow> dataTableRows = new ArrayList<DataTableRow>();
+
+        private int counter = 0;
+
+        @Override
+        public boolean processLine(final String line) throws IOException {
+            final Splitter splitter = Splitter.on(SEPARATOR);
+            final Iterable<String> cells = splitter.split(line);
+            DataTableRow dataTableRow = new DataTableRow(new ArrayList<Comment>(), Lists.newArrayList(cells), counter);
+            dataTableRows.add(dataTableRow);
+            counter++;
+            return true;
+        }
+
+        @Override
+        public List<DataTableRow> getResult() {
+            return dataTableRows;
+        }
+    };
 }
---
+++ src/test/java/fixtures/common/transformers/FileTransformerTest.java
@@ -0,0 +1,101 @@
+package fixtures.common.transformers;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+import com.google.common.io.LineProcessor;
+import cucumber.runtime.CucumberException;
+import cucumber.table.DataTable;
+import gherkin.formatter.model.Comment;
+import gherkin.formatter.model.DataTableRow;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FileTransformerTest {
+    private DataTable dataTable;
+
+    @Before
+    public void setUp() throws Exception {
+        List<String> dataTableToCompareHeader = Lists.newArrayList("type de bien", "nombre de pieces");
+        List<String> data = Lists.newArrayList("appartement", "5");
+        List<DataTableRow> rows = Lists
+                .newArrayList(new DataTableRow(new ArrayList<Comment>(), dataTableToCompareHeader, 1),
+                        new DataTableRow(new ArrayList<Comment>(), data, 2));
+        dataTable = new DataTable(rows, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFileTransformer_file_null() throws Exception {
+
+        File file = null;
+        //
+        FileTransformer fileTransformer = new FileTransformer(dataTable);
+        fileTransformer.toDataTable(file);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFileTransformer_file_doesnt_exist() throws Exception {
+
+        File file = new File("unknow.jstl");
+        //
+        FileTransformer fileTransformer = new FileTransformer(dataTable);
+        fileTransformer.toDataTable(file);
+    }
+
+    @Test
+    public void testFileTransformer_file_exist() throws Exception {
+
+        File file = new File(getClass().getClassLoader().getResource("files/file_to_transform.txt").getFile());
+        //
+        FileTransformer fileTransformer = new FileTransformer(dataTable);
+        final DataTable dataTableFromFile = fileTransformer.toDataTable(file);
+        List<List<String>> raw = dataTableFromFile.raw();
+        //
+        List<List<String>> expected = Lists.<List<String>>newArrayList(Lists.<String>newArrayList("ID", "FILE", "REF"),
+                Lists.<String>newArrayList("12", "fs.sys", "1878D"),
+                Lists.<String>newArrayList("478", "read.txt", "2574"));
+        assertThat(raw, is(expected));
+    }
+
+    @Test
+    public void testFileTransformer_file_exist_with_no_tab_separator() throws Exception {
+
+        File file = new File(getClass().getClassLoader().getResource("files/file_to_transform_no_tab.txt").getFile());
+        //
+        FileTransformer fileTransformer = new FileTransformer(dataTable);
+        final DataTable dataTableFromFile = fileTransformer.toDataTable(file);
+        List<List<String>> raw = dataTableFromFile.raw();
+        //
+        List<List<String>> expected = Lists.<List<String>>newArrayList(Lists.<String>newArrayList("ID  FILE     REF"),
+                Lists.<String>newArrayList("1   fs.sys   878D"), Lists.<String>newArrayList("478 read.txt 2574"));
+        assertThat(raw, is(expected));
+    }
+
+    @Test(expected = CucumberException.class)
+    public void testFileTransformer_throw_IOException() throws Exception {
+
+        File file = new File(getClass().getClassLoader().getResource("files/file_to_transform.txt").getFile());
+        LineProcessor<List<DataTableRow>> lineProcessor = mock(LineProcessor.class);
+        //
+        when(lineProcessor.processLine(anyString())).thenThrow(new IOException("fake exception"));
+        //
+        FileTransformer fileTransformer = new FileTransformer(dataTable);
+        FileTransformer spyFileTransformer = spy(fileTransformer);
+        //
+        doReturn(lineProcessor).when(spyFileTransformer).newLineProcessor();
+        //
+        final DataTable dataTableFromFile = spyFileTransformer.toDataTable(file);
+        dataTableFromFile.raw();
+    }
+}
