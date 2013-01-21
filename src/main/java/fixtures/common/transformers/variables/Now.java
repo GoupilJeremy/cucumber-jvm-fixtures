35a1eba5f4b331f87c19b60622e722cbd8c37721
lorrenzo <ltricard@gmail.com>
2013-01-21 09:07:27 +0100

    [LAT] ajoute des fonctionnalités sur la variable now

--- src/main/java/fixtures/common/transformers/variables/Now.java
+++ src/main/java/fixtures/common/transformers/variables/Now.java
@@ -4,10 +4,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.google.common.base.Function;
-import com.google.common.base.Predicate;
-import com.google.common.base.Splitter;
-import com.google.common.base.Strings;
+import com.google.common.base.*;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import org.joda.time.DateTime;
@@ -17,13 +14,19 @@ import org.slf4j.LoggerFactory;
 public class Now implements Function<String, String> {
     private static final Logger LOGGER = LoggerFactory.getLogger(Now.class);
 
+	private final static CharMatcher CLEAN_PATTERN = CharMatcher.anyOf("+myMY").precomputed();
+
     public static final String DEFAULT_PATTERN = "yyyy/MM/dd";
 
     private static final String NOW = "now";
 
-    private static final Pattern PATTERN = Pattern.compile("\\$\\{(" + NOW + ".*)\\}");
+    private static final Pattern PATTERN = Pattern.compile("\\$\\{("+NOW+"[ +\\-_:/;\\w()]*)}");
 
     private static final Pattern ADD_DAYS_PATTERN = Pattern.compile("^[-+]{1}[0-9]+$");
+
+    private static final Pattern ADD_MONTH_PATTERN = Pattern.compile("^[-+]{1}[0-9]{1,3}[mM]{1}$");
+
+    private static final Pattern ADD_YEAR_PATTERN = Pattern.compile("^[-+]{1}[0-9]{1,2}[yY]{1}$");
 
 	private static final Splitter SPLITTER = Splitter.on(" ");
 
@@ -32,22 +35,27 @@ public class Now implements Function<String, String> {
         String strToProceed = Strings.nullToEmpty(input);
         String datePattern = "";
         Matcher matcher = PATTERN.matcher(strToProceed);
-        boolean found = matcher.find();
-        if (found) {
-            try {
-                String group = matcher.group(1);
+        while (matcher.find()) {
+	        String group = matcher.group(1);
+	        try {
                 Iterable<String> groupSplitted = SPLITTER.split(group);
                 //
                 checkStart(group);
-                int offset = getOffset(groupSplitted);
+                int offsetDays = getOffsetFromPattern(groupSplitted, ADD_DAYS_PATTERN);
+                int offsetMonth = getOffsetFromPattern(groupSplitted, ADD_MONTH_PATTERN);
+                int offsetYear = getOffsetFromPattern(groupSplitted, ADD_YEAR_PATTERN);
                 datePattern = getDatePattern(group);
-                DateTime now = DateTime.now().plusDays(offset);
-                strToProceed = matcher.replaceAll(now.toString(datePattern));
+                DateTime now = DateTime.now().plusDays(offsetDays).plusMonths(offsetMonth).plusYears(offsetYear);
+	            strToProceed = strToProceed.replace("${" + group + "}", now.toString(datePattern));
             } catch (VariableException ve) {
-                LOGGER.error("Mauvais pattern", ve);
+	            LOGGER.error("Mauvais pattern", ve);
+	            strToProceed = strToProceed.replace("${" + group + "}", "$bad{" + group + "}");
             } catch (IllegalArgumentException ie) {
-                LOGGER.error("Illegal pattern pour la date {}", datePattern, ie);
+	            LOGGER.error("Illegal pattern pour la date {}", datePattern, ie);
+	            strToProceed = strToProceed.replace("${" + group + "}", "$bad{" + group + "}");
             }
+	        // mise à jour du matcher avec la nouvelle chaine de caractère
+	        matcher.reset(strToProceed);
         }
         return strToProceed;
     }
@@ -85,18 +93,17 @@ public class Now implements Function<String, String> {
         return group.substring(first + 1, second);
     }
 
-    private int getOffset(final Iterable<String> groupSplitted) {
-
+    private int getOffsetFromPattern(final Iterable<String> groupSplitted, Pattern pattern) {
         List<String> listPatternDays = Lists
-                .newArrayList(Iterables.filter(groupSplitted, new PatternMatcherPredicate(ADD_DAYS_PATTERN)));
+                .newArrayList(Iterables.filter(groupSplitted, new PatternMatcherPredicate(pattern)));
         if (listPatternDays == null || listPatternDays.size() == 0) {
             return 0;
         } else if (listPatternDays.size() == 1) {
             // "+" non géré pour le moment (peut-être en JDK 7)
-            return Integer.parseInt(listPatternDays.get(0).trim().replace("+", ""));
+            return Integer.parseInt(CLEAN_PATTERN.removeFrom(listPatternDays.get(0).trim()));
         } else {
             throw new VariableException(
-                    "Trop de pattern pour l'ajout de jour (1 seul doit être précisé): " + listPatternDays.toString());
+                    "Trop de pattern (1 seul doit être précisé): " + listPatternDays.toString());
         }
     }
 
--- src/test/java/fixtures/common/transformers/variables/NowTest.java
+++ src/test/java/fixtures/common/transformers/variables/NowTest.java
@@ -35,6 +35,30 @@ public class NowTest {
         assertThat(result, is("ma date " + expectedMonth + " affichée"));
     }
 
+	@Test
+    public void testMonth_input_has_many_var() throws Exception {
+        String input = "ma date ${now} et ${now +5} affichée";
+        Now now = new Now();
+        String result = now.apply(input);
+        String expectedDate = DateTime.now().toString(Now.DEFAULT_PATTERN);
+        String expectedDate2 = DateTime.now().plusDays(5).toString(Now.DEFAULT_PATTERN);
+        //
+        assertThat(result, is("ma date " + expectedDate + " et "+expectedDate2+ " affichée"));
+    }
+
+	@Test
+    public void testMonth_input_has_many_date_with_a_bad_one() throws Exception {
+        String input = "ma date ${now -2m}, ${now +5 +6} et ${now (yy/MM-EEEE) +5} affichée";
+        Now now = new Now();
+        String result = now.apply(input);
+        String expectedDate = DateTime.now().plusMonths(-2).toString(Now.DEFAULT_PATTERN);
+        String expectedDate2 = DateTime.now().plusDays(5).toString("yy/MM-EEEE");
+        //
+        assertThat(result, is("ma date " + expectedDate + ", $bad{now +5 +6} et "+expectedDate2+ " affichée"));
+    }
+
+	// day
+
     @Test
     public void testMonth_input_has_var_minus_one_month() throws Exception {
         String input = "ma date ${now -1} affichée";
@@ -92,7 +116,7 @@ public class NowTest {
         String result = now.apply(input);
         String expectedMonth = DateTime.now().toString("yyyy/MM/dd hh:mm");
         //
-        assertThat(result, is("ma date ${now (yyyy/MM/dd hh:mm)(yyyy/MM/dd)} affichée"));
+        assertThat(result, is("ma date $bad{now (yyyy/MM/dd hh:mm)(yyyy/MM/dd)} affichée"));
     }
 
     @Test
@@ -108,19 +132,21 @@ public class NowTest {
     @Test
     public void testMonth_input_has_no_var_many_date_pattern() throws Exception {
         String input = "ma date ${now (yyyy/MM/dd-hh:mm) (yyyy)} affichée";
+        String expected = "ma date $bad{now (yyyy/MM/dd-hh:mm) (yyyy)} affichée";
         Now now = new Now();
         String result = now.apply(input);
         //
-        assertThat(result, is(input));
+        assertThat(result, is(expected));
     }
 
     @Test
     public void testMonth_input_has_no_var_badd_pattern() throws Exception {
         String input = "ma date ${now (lololo)} affichée";
+        String expected = "ma date $bad{now (lololo)} affichée";
         Now now = new Now();
         String result = now.apply(input);
         //
-        assertThat(result, is(input));
+        assertThat(result, is(expected));
     }
 
     @Test
@@ -136,19 +162,21 @@ public class NowTest {
     @Test
     public void testMonth_input_has_var_plus_two_month_many_time() throws Exception {
         String input = "ma date ${now +2 -1} affichée";
+        String expected = "ma date $bad{now +2 -1} affichée";
         Now now = new Now();
         String result = now.apply(input);
         //
-        assertThat(result, is(input));
+        assertThat(result, is(expected));
     }
 
     @Test
     public void testMonth_input_has_var_plus_two_month_without_space() throws Exception {
         String input = "ma date ${now+2} affichée";
+        String expected = "ma date $bad{now+2} affichée";
         Now now = new Now();
         String result = now.apply(input);
         //
-        assertThat(result, is(input));
+        assertThat(result, is(expected));
     }
 
     @Test
@@ -189,5 +217,167 @@ public class NowTest {
         String expectedMonth = DateTime.now().toString(Now.DEFAULT_PATTERN);
         //
         assertThat(result, is("ma date " + expectedMonth + " affichée"));
+    }
+
+	// month
+
+	@Test
+    public void testMonth_input_add_month() throws Exception {
+        String input = "ma date ${now +2m} affichée";
+        Now now = new Now();
+        String result = now.apply(input);
+        String expectedMonth = DateTime.now().plusMonths(2).toString(Now.DEFAULT_PATTERN);
+        //
+        assertThat(result, is("ma date " + expectedMonth + " affichée"));
+    }
+
+
+	@Test
+    public void testMonth_input_minus_month() throws Exception {
+        String input = "ma date ${now -3m} affichée";
+        Now now = new Now();
+        String result = now.apply(input);
+        String expectedMonth = DateTime.now().minusMonths(3).toString(Now.DEFAULT_PATTERN);
+        //
+        assertThat(result, is("ma date " + expectedMonth + " affichée"));
+    }
+
+
+
+	@Test
+    public void testMonth_input_add_month_and_pattern() throws Exception {
+        String input = "ma date ${now (yyyy/MM/dd-hh:mm) +3m} affichée";
+        Now now = new Now();
+        String result = now.apply(input);
+        String expectedMonth = DateTime.now().plusMonths(3).toString("yyyy/MM/dd-hh:mm");
+        //
+        assertThat(result, is("ma date " + expectedMonth + " affichée"));
+    }
+
+
+	@Test
+    public void testMonth_input_add_month_day_and_pattern() throws Exception {
+        String input = "ma date ${now (yyyy/MM/dd-hh:mm) +25 +3m} affichée";
+        Now now = new Now();
+        String result = now.apply(input);
+        String expectedMonth = DateTime.now().plusDays(25).plusMonths(3).toString("yyyy/MM/dd-hh:mm");
+        //
+        assertThat(result, is("ma date " + expectedMonth + " affichée"));
+    }
+
+	@Test
+    public void testMonth_input_add_month_day_and_pattern_different_order() throws Exception {
+        String input = "ma date ${now +5m -26 (yyyy/MM/dd-mm)} affichée";
+        Now now = new Now();
+        String result = now.apply(input);
+        String expectedMonth = DateTime.now().plusDays(-26).plusMonths(5).toString("yyyy/MM/dd-mm");
+        //
+        assertThat(result, is("ma date " + expectedMonth + " affichée"));
+    }
+
+	@Test
+    public void testMonth_input_add_month_in_upper_case() throws Exception {
+        String input = "ma date ${now +5M (yyyy/MM/dd-mm)} affichée";
+        Now now = new Now();
+        String result = now.apply(input);
+        String expectedMonth = DateTime.now().plusMonths(5).toString("yyyy/MM/dd-mm");
+        //
+        assertThat(result, is("ma date " + expectedMonth + " affichée"));
+    }
+
+	@Test
+    public void testMonth_input_has_two_month_pattern() throws Exception {
+        String input = "ma date $bad{now +2m -1m} affichée";
+        String expected = "ma date $bad{now +2m -1m} affichée";
+        Now now = new Now();
+        String result = now.apply(input);
+        //
+        assertThat(result, is(expected));
+    }
+
+	// year
+
+	@Test
+    public void testMonth_input_add_year() throws Exception {
+        String input = "ma date ${now +2y} affichée";
+        Now now = new Now();
+        String result = now.apply(input);
+        String expectedMonth = DateTime.now().plusYears(2).toString(Now.DEFAULT_PATTERN);
+        //
+        assertThat(result, is("ma date " + expectedMonth + " affichée"));
+    }
+
+
+	@Test
+    public void testMonth_input_minus_year() throws Exception {
+        String input = "ma date ${now -3y} affichée";
+        Now now = new Now();
+        String result = now.apply(input);
+        String expectedMonth = DateTime.now().minusYears(3).toString(Now.DEFAULT_PATTERN);
+        //
+        assertThat(result, is("ma date " + expectedMonth + " affichée"));
+    }
+
+
+
+	@Test
+    public void testMonth_input_add_year_and_pattern() throws Exception {
+        String input = "ma date ${now (yyyy/MM/dd-hh:mm) +3y} affichée";
+        Now now = new Now();
+        String result = now.apply(input);
+        String expectedMonth = DateTime.now().plusYears(3).toString("yyyy/MM/dd-hh:mm");
+        //
+        assertThat(result, is("ma date " + expectedMonth + " affichée"));
+    }
+
+
+	@Test
+    public void testMonth_input_add_year_day_and_pattern() throws Exception {
+        String input = "ma date ${now (yyyy/MM/dd-hh:mm) +25 +3y} affichée";
+        Now now = new Now();
+        String result = now.apply(input);
+        String expectedMonth = DateTime.now().plusDays(25).plusYears(3).toString("yyyy/MM/dd-hh:mm");
+        //
+        assertThat(result, is("ma date " + expectedMonth + " affichée"));
+    }
+
+	@Test
+    public void testMonth_input_add_year_day_and_pattern_different_order() throws Exception {
+        String input = "ma date ${now +5y -26 (yyyy/MM/dd-mm)} affichée";
+        Now now = new Now();
+        String result = now.apply(input);
+        String expectedMonth = DateTime.now().plusDays(-26).plusYears(5).toString("yyyy/MM/dd-mm");
+        //
+        assertThat(result, is("ma date " + expectedMonth + " affichée"));
+    }
+
+	@Test
+    public void testMonth_input_add_year_in_upper_case() throws Exception {
+        String input = "ma date ${now +5Y (yyyy/MM/dd-mm)} affichée";
+        Now now = new Now();
+        String result = now.apply(input);
+        String expectedMonth = DateTime.now().plusYears(5).toString("yyyy/MM/dd-mm");
+        //
+        assertThat(result, is("ma date " + expectedMonth + " affichée"));
+    }
+
+	@Test
+    public void testMonth_input_add_year_month_day_in_upper_case() throws Exception {
+        String input = "ma date ${now +5Y +254 -5m (yyyy/MM/dd-mm)} affichée";
+        Now now = new Now();
+        String result = now.apply(input);
+        String expectedMonth = DateTime.now().plusDays(254).plusMonths(-5).plusYears(5).toString("yyyy/MM/dd-mm");
+        //
+        assertThat(result, is("ma date " + expectedMonth + " affichée"));
+    }
+
+	@Test
+    public void testMonth_input_has_two_year_pattern() throws Exception {
+        String input = "ma date $bad{now +2y -1y} affichée";
+        String expected = "ma date $bad{now +2y -1y} affichée";
+        Now now = new Now();
+        String result = now.apply(input);
+        //
+        assertThat(result, is(expected));
     }
 }
